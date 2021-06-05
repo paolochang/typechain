c159dc518abb55a076b4bebb2fc215b85017d03c
Paolo Chang <chanhwan.chang@gmail.com>
2021-06-04 22:36:15 -0400

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Paolo",
+  age: 30,
+  gender: "male",
 };
 
-console.log(sayHi("Paolo", 30));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
+};
+
+console.log(sayHi(person));
 
 export {};
